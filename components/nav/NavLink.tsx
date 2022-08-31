164037f5ab46a936ce78d9919e489a5512b46d5f
Mohamed Nehad <mohammednehad550@gmail.com>
2022-08-31 14:11:23 +0200

    feat: Make Navbar responsive

--- components/nav/NavLink.tsx
+++ components/nav/NavLink.tsx
@@ -7,7 +7,12 @@ interface NavLinkProps {
 
 const NavLink: FC<NavLinkProps> = ({ href, title }) => {
     return (
-        <button className="mx-4 h-full text-dark-grayish-blue hover:text-dark-blue border-b-4 border-transparent hover:border-primary-color-1" >
+        <button
+            className={`
+                m-4 ml-6 border-b-4 border-transparent self-start font-bold hover:text-dark-grayish-blue
+                md:my-0 md:mx-4 md:h-full md:text-dark-grayish-blue md:hover:text-dark-blue md:hover:border-primary-color-1 md:font-normal
+            `}
+        >
             <a href={href}>
                 {title}
             </a>
--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,19 +1,57 @@
-import Image from "next/image";
-import { FC, PropsWithChildren, ReactNode } from "react";
-import { IconCart } from "../icons";
+import { FC, PropsWithChildren, ReactNode, useState } from "react";
+import ReactModal from "react-modal";
+import { IconClose, IconMenu } from "../icons";
 
 
 
 const Navbar: FC<PropsWithChildren<{ logo: ReactNode, actions: ReactNode }>> = ({ children, logo, actions }) => {
 
+    const [opened, setOpened] = useState(true)
 
     return (
-        <nav className="w-100% h-28 flex flex-row items-center border-b-2">
+        <nav className="w-100% md:h-28 h-[4.5rem] flex flex-row items-center border-b-2">
+
+            <ReactModal
+                isOpen={opened}
+                onRequestClose={() => setOpened(false)}
+                shouldCloseOnEsc
+                shouldCloseOnOverlayClick
+                overlayClassName={"md:hidden absolute top-0 left-0 w-screen h-screen"}
+                style={{
+                    overlay: {
+                        background: "rgba(0,0,0,75%)",
+                    },
+                    content:
+                    {
+                        border: 0,
+                        left: 0,
+                        top: 0,
+                        padding: 0,
+                        height: '100vh',
+                        width: "70%",
+                    }
+                }}
+            >
+                <div className={`bg-white flex flex-col items-start h-full `} >
+                    <button
+                        onClick={() => setOpened(false)}
+                        className="md:hidden p-6 pb-7 flex fill-dark-grayish-blue"
+                    >
+                        <IconClose />
+                    </button>
+                    {children}
+                </div>
+            </ReactModal>
+            <button onClick={() => setOpened(true)} className="md:hidden p-6">
+                <IconMenu />
+            </button>
             <a href="#" className="mr-14">
                 {logo}
             </a>
-            {children}
-            <span className='ml-auto flex flex-row'>
+            <span className="h-full hidden md:block">
+                {children}
+            </span>
+            <span className='ml-auto md:mr-0 mx-4 flex flex-row'>
                 {actions}
             </span>
         </nav>
