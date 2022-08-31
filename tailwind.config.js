fdf9d75e417b318f85877e329268cc032629680c
Mohamed Nehad <mohammednehad550@gmail.com>
2022-08-31 12:03:46 +0200

    feat: Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color-1": "var(--orange)",
+        "primary-color-2": "var(--pale-orange)",
+        'dark-blue': ' hsl(220, 13%, 13%)',
+        "dark-grayish-blue": " hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+      },
+    },
   },
   plugins: [],
 }
